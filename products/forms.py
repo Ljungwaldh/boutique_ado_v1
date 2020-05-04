c5b44b8d77d9fd96c64b6a2b1d25e0823a44a888
Ljungwaldh <gustaf.ljungwaldh@live.co.uk>
2020-05-04 07:03:12 +0000

    Added product form

---
+++ products/forms.py
@@ -0,0 +1,17 @@
+from django import forms
+from .models import Product, Category
+
+class ProductForm(forms.ModelForm):
+
+    class Meta:
+        model = Product
+        fields = '__all__'
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        categories = Categroy.objects.all()
+        friendly_names = [(c.id, c.get_friendly_name()) for c in categories]
+
+        self.fields['category'].choices = friendly_names
+        for field_name, field in self.field.items():
+            field.widgt.attrs['class'] = 'border 0-black rounded-0'
