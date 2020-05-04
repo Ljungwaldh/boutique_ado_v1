2472aeb21564ece167ddc20dffb709457c8407bc
Ljungwaldh <gustaf.ljungwaldh@live.co.uk>
2020-05-04 12:17:30 +0000

    Further adjustment for settings for heroku

--- boutique_ado/settings.py
+++ boutique_ado/settings.py
@@ -21,10 +21,10 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '(_m#pe+=28!j--1u9df$l#c-n59qhtvu%6&154$7ftv49p4noy'
+SECRET_KEY = os.environ.get('SECRET_KEY', '')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = 'DEVELOPMENT' in os.environ
 
 ALLOWED_HOSTS = ['ljungwaldh-boutique-ado.herokuapp.com', 'localhost']
 
